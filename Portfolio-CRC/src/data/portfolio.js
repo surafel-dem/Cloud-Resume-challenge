7c23993ac20ef8cde9c025e376e07406993796bb
Sura <surafel.demissie@outlook.com>
2023-06-09 16:34:18 +0100

    test

--- Portfolio-CRC/src/data/portfolio.js
+++ Portfolio-CRC/src/data/portfolio.js
@@ -6,7 +6,7 @@ export default [
     link: "https://www.ehealth-embark.ie/",
   },
   {
-    title: "Cloud Resume Challenge",
+    title: "Cloud Resume Challenge Forrest",
     imgUrl: "/assets/crc.png",
     stack: ["React", "AWS", "JavaScript"],
     link: "https://surafeldemissie.me/",
